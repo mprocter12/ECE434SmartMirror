74ec0e09a0ab0e87a3288bc1afd7fa14be6c2537
Andy Barbour <barbouat@rose-hulman.edu>
2019-11-17 17:02:07 -0500

    Testing Blynk

--- basicstreamer.py
+++ basicstreamer.py
@@ -52,7 +52,7 @@ def weather_icon(ds_icon):
 # Virtual Pin Handler
 @blynk.handle_event('write V0')
 def write_virtual_pin_handler(pin, value):
-	print("Pin Value: ", value)
+	print("[WRITE_VIRTUAL_PIN_EVENT] Pin: V{} Value: '{}'".format(pin, value))
 	if(value == 0):
 		subprocess.call("./turnOffMonitor.sh") # Turns off the HDMI port and is turned back on with a keypress
 		print('Turning Off Monitor ...')
